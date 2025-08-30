805b6a596512ff16e3d48bbd73ab76bdb3ff4cd0
KatharineAC <katharineandrade94@gmail.com>
2025-08-30 16:02:37 -0300

    SQlite3 implementation for Score using proxy design pattern

--- code/Background.py
+++ code/Background.py
@@ -1,11 +1,14 @@
+import pygame
 
 from code.Entity import Entity
-from code.const import WIN_WIDTH, ENTITY_SPEED
+from code.const import WIN_WIDTH, ENTITY_SPEED, WIN_HEIGHT
 
 
 class Background(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.image = pygame.transform.scale(self.image, (WIN_WIDTH, WIN_HEIGHT))
+        self.rect = self.image.get_rect(topleft = position)
 
 
     def move(self, ):
--- code/Entity.py
+++ code/Entity.py
@@ -2,14 +2,20 @@ from abc import ABC, abstractmethod
 
 import pygame.image
 
-from code.const import ENTITY_HEALTH, ENTITY_DAMAGE, ENTITY_SCORE
+from code.const import ENTITY_HEALTH, ENTITY_DAMAGE, ENTITY_SCORE, WIN_WIDTH, WIN_HEIGHT
 
 
 class Entity(ABC):
-    def __init__(self, name: str, position: tuple):
+    def __init__(self, name: str, position: tuple, load_image=True):
+        super().__init__()
         self.name = name
-        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
-        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.image = None
+        self.rect = None
+
+        if load_image:
+            self.image = pygame.image.load('./asset/' + name + '.png').convert_alpha()
+            self.rect = self.image.get_rect(left = position[0], top = position[1])
+
         self.speed = 0
         self.health = ENTITY_HEALTH[self.name]
         self.damage = ENTITY_DAMAGE[self.name]
